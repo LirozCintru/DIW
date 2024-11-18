ef0f0895d77c9d12207fa61ee830e0d637d207ce
LirozCintru <javiercintru2001@gmail.com>
2024-11-18 19:06:23 +0100

    Y ahora sin ti no soy nada

--- PracticaT4/script.js
+++ PracticaT4/script.js
@@ -1,31 +1,8 @@
 var video = document.getElementById("myVideo");
 var btn = document.getElementById("myBtn");
 
-var soundBtn = document.getElementById("soundBtn");
-var soundBtn = document.getElementById("soundImg");
-
-var videoTitle = document.getElementById("videoTitle");
-var showTime = 2;
-
-video.addEventListener("timeupdate", function() {
-    if (video.currentTime >= showTime && video.currentTime < showTime + 20) {
-        videoTitle.style.display = "block";
-    } else {
-        videoTitle.style.display = "none";
-    }
-});
-
-function soundMute() {
-    if (video.muted) {
-        video.muted = false;
-        soundImg.src = "soundB.png";
-        soundImg.alt = "Sound On";
-    } else {
-        video.muted = true;
-        soundImg.src = "muteB.png";
-        soundImg.alt = "Sound Off";
-    }
-}
+// var soundBtn = document.getElementById("soundBtn");
+// var soundBtn = document.getElementById("soundImg");
 
 function myFunction() {
     if (video.paused) {
