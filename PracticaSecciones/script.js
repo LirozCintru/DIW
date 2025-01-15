d318c431f9f5d44385832bcbeccccbde84dfbece
LirozCintru <javiercintru2001@gmail.com>
2025-01-15 21:21:55 +0100

    Hub

--- PracticaSecciones/script.js
+++ PracticaSecciones/script.js
@@ -1,1 +1,23 @@
+let secciones = document.querySelectorAll("section");
 
+secciones.forEach(seccion => {
+    seccion.addEventListener("click", (ev) => {
+        seccion.classList.add("is-expanded");
+        secciones.forEach(element => {
+            if(element!=seccion){
+                element.classList.add("not-expanded");
+            }
+        });
+        let x = seccion.getElementsByClassName("cerral");
+        let elemento = x[0];
+        elemento.addEventListener("click", (ev) => {
+            ev.stopPropagation();
+            seccion.classList.remove("is-expanded");
+            secciones.forEach(element => {
+                if(element!=seccion){
+                    element.classList.remove("not-expanded");
+                }
+            });
+        });
+    });
+});
